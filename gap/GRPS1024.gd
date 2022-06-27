2d1f1aa157ee51fee0c43e6e3c18eec6021a8764
David <1234753+davidburrell@users.noreply.github.com>
2022-06-27 14:40:27 -0400

    added .gitignore

--- gap/GRPS1024.gd
+++ gap/GRPS1024.gd
@@ -1,27 +1,48 @@
 
 # GRPS1024: Utilities for iterating through the groups of order 1024 with p-class at least 3
 #
-#! @Chapter Introduction
+#! @Chapter Groups of Order 1024
+#! @Section Overview
 #!
-#! GRPS1024 is a package which does some
-#! interesting and cool things
+#! This package gives access to all of the groups of order 1024 with p-class 3 and greater. The groups are sorted first by their parent group ids and then by the pc codes of the standard presentations for the groups. These groups were used in the 2021 enumeration of the groups of order 1024 <Cite Key="Burrell2021a"/>
 #!
+#!
+#!
+
+DeclareGlobalFunction("ReloadGRPS1024");
 #! @Chapter Functionality
 #!
 #!
-#! @Section Example Methods
+#! @Section Methods
 #!
-#! This section will describe the example
-#! methods of GRPS1024
-
+#! This section will describe the functions available in GRPS1024
+#!
 #! @Description
-#!   Insert documentation for your function here
-
-DeclareGlobalFunction("ReloadGRPS1024");
+#! Computes the list of lists of degrees of ordinary characters
+#! associated to the $p$-blocks of the group $G$
+#! with $p$-modular character table <A>modtbl</A>
+#! and underlying ordinary character table `ordtbl`.
+#! @Returns a list
+#! @Arguments modtbl
+##! @Group CharacterDegreesOfBlocks
+#! @Label chardegblocks
+#! @ChapterInfo Functionality, Methods
 DeclareGlobalFunction("NumDescendants");
+#! @Description
+#! a description
 DeclareGlobalFunction("LoadDescendants");
+#! @Description
+#! a description
 DeclareGlobalFunction("CheckoutDescendants");
+#! @Description
+#! a description
 DeclareGlobalFunction("IsAvailable");
+#! @Description
+#! a description
 DeclareGlobalFunction("FindGroupN");
+#! @Description
+#! a description
 DeclareGlobalFunction("FindNthAvailableGroup");
+#! @Description
+#! a description
 DeclareGlobalFunction("AvailableMap");
--- makedoc.g
+++ makedoc.g
@@ -1,10 +1,23 @@
+# AutoDoc: Generate documentation from GAP source code
 #
-# GRPS1024: Utilities for iterating through the groups of order 1024 with p-class at least 3
+# Copyright of AutoDoc belongs to its developers.
+# Please refer to the COPYRIGHT file for details.
 #
-# This file is a script which compiles the package manual.
-#
-if fail = LoadPackage("AutoDoc", "2018.02.14") then
-    Error("AutoDoc version 2018.02.14 or newer is required.");
-fi;
+# SPDX-License-Identifier: GPL-2.0-or-later
 
-AutoDoc( rec( scaffold := true, autodoc := true ) );
+LoadPackage("AutoDoc");
+
+AutoDoc( rec( 
+    autodoc := true,
+    gapdoc := rec(
+        LaTeXOptions := rec( EarlyExtraPreamble := """
+            \usepackage{a4wide}
+            \newcommand{\bbZ}{\mathbb{Z}}
+        """ )
+    ),
+    scaffold := rec(
+        # includes := [ "overview.xml"],
+        bib := "manualbib.xml"
+    )
+));
+QUIT;
